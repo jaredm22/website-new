db04e1e7aba1956e6b4187ca8b1cfd790ec1bd14
Jared Min <jkymin2@gmail.com>
2022-01-30 20:38:15 -0500

    Completed about me section

--- pages/components/Message.js
+++ pages/components/Message.js
@@ -1,29 +1,28 @@
 import Image from "next/image";
 import rpod from '../../public/rpod.png'
-// import jjcafe from '../../public/jjcafe.png'
-
+import pixel from '../../public/pixelate.png'
 
 export default function Message(props) {
 
     console.log(props)
 
-    return(
-        props.image ? 
+    if (props.image == "RPOD") {
+        return(
             <a className={`link-message ${props.type} ${props.last ? "last" : ""}`} href={"https://rpod-tracker.vercel.app/"}>
-                <div className="message-top">
-                    <Image src={rpod} layout="fill"/>
-                </div>
-
-                <div className="message-bottom">
-                    {props.text}
-                </div>
+                <Image src={rpod} height={300} width={400}/>
             </a>
-
-            :
+        )
+    } else if (props.image == 'pixel') {
+        return(
+            <a className={`link-message ${props.type} ${props.last ? "last" : ""}`} href={"https://pixel-art-chi.vercel.app/"}>
+                <Image src={pixel} height={300} width={400}/>
+            </a>
+        )
+    } else {
+        return(
             <div className={`message ${props.type} ${props.last ? "last" : ""}`}>
                 {props.text}
             </div>
-            
-            
-    );
+        )
+    }
 }
